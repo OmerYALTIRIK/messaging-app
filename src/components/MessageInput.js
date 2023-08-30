3f059944f558a7fdd3fd79583c67db0d7eb23243
Ömer YALTIRIK <omerfarukyaltirik@gmail.com>
2023-08-30 17:14:45 +0300

    added tailwindcss classes

--- src/components/MessageInput.js
+++ src/components/MessageInput.js
@@ -1,28 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function MessageInput({ sendMessage }) {
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
 
   const handleMessageChange = (event) => {
     setMessage(event.target.value);
   };
 
   const handleKeyDown = (event) => {
-    if (event.key === 'Enter') {
+    if (event.key === "Enter") {
       sendMessage(message);
-      setMessage('');
+      setMessage("");
     }
   };
 
   return (
-    <div>
+    <div class="relative flex w-full flex-wrap items-stretch mb-3">
       <input
         type="text"
         value={message}
         onChange={handleMessageChange}
         onKeyDown={handleKeyDown} // Enter tuşuna basıldığında handleKeyDown çalışacak
-        className='border-2 border-gray-300 p-2 max-w-lg rounded-md'
+        placeholder="Mesajınızı yazınız..."
+        className="px-3 py-3 placeholder-slate-300 text-slate-600 relative bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full pr-10"
       />
+      <span class="z-10 h-full leading-snug font-normal absolute text-center text-slate-300 bg-transparent rounded text-base items-center justify-center w-8 right-0 pr-3 py-3">
+        <i class="fas fa-user"></i>
+      </span>
       <button onClick={() => sendMessage(message)}>Gönder</button>
     </div>
   );
