239e882a1d681cb7da090dd3009c111b7b1b4d7f
Ömer YALTIRIK <omerfarukyaltirik@gmail.com>
2023-08-31 21:28:23 +0300

    Added styles

--- src/components/MessageInput.js
+++ src/components/MessageInput.js
@@ -15,19 +15,19 @@ function MessageInput({ sendMessage }) {
   };
 
   return (
-    <div class="relative flex w-full flex-wrap items-stretch mb-3">
+    <div class="grid gap-4 grid-cols-2 grid-rows-2 w-2/6 mb-3 items-stretch">
       <input
         type="text"
         value={message}
         onChange={handleMessageChange}
         onKeyDown={handleKeyDown} // Enter tuşuna basıldığında handleKeyDown çalışacak
         placeholder="Writing Message..."
-        className="px-3 py-3 placeholder-slate-300 text-slate-600 relative bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full pr-10"
+        className=" placeholder-slate-300 text-slate-600 bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring"
       />
-      <span class="z-10 h-full leading-snug font-normal absolute text-center text-slate-300 bg-transparent rounded text-base items-center justify-center w-8 right-0 pr-3 py-3">
+      <span class="z-10 h-full leading-snug font-normal text-center text-slate-300 bg-transparent rounded text-base w-8 py-3">
         <i class="fas fa-user"></i>
       </span>
-      <button onClick={() => sendMessage(message)}>Gönder</button>
+      <button onClick={() => sendMessage(message)} className="bg-sky-500 outline outline-offset-2 outline-cyan-500 rounded-lg">Gönder</button>
     </div>
   );
 }
